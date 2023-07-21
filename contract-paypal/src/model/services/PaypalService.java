0c7444ccf51919f8a178127f85d61804e09764fe
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-07-21 15:08:25 -0300

    PayPal Payments Logic

--- contract-paypal/src/model/services/PaypalService.java
+++ contract-paypal/src/model/services/PaypalService.java
@@ -4,12 +4,12 @@ public class PaypalService implements OlinePaymentService{
 
     @Override
     public Double paymentFee(Double amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return amount * 0.2f;
     }
 
     @Override
     public Double insterest(Double amount, Double months) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return amount * 0.1f * months;
     }
     
 }
